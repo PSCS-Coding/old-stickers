2f86415201b477be9b1dd2028a52066d8878bd5d
Milo Rupp <milo.rupp@pscs.org>
2015-03-06 14:45:07 -0800

    added the insert

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 include_once("rss.php");
+include_once("connection.php");
+
 $rssInfo = getRss();
 
 foreach ($rssInfo as $class) {
@@ -7,5 +9,10 @@ foreach ($rssInfo as $class) {
 	print_r($class);
 	//echo $class['title'] . " " . $class[''] . "<br />";
 	echo "</pre>";
+	$stmt = $db_stickers->prepare("INSERT INTO classes (classname) VALUES (?)");
+    $stmt->bind_param('s', $class['title']);
+    $stmt->execute();
+    $stmt->close();
+
 }
 ?>

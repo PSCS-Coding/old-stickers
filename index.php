3a23ca5d4baf7ee656fd691fefdb4fffa7a68c4a
Milo Rupp <milo.rupp@pscs.org>
2015-03-06 14:51:49 -0800

    moved the rss init function to the rss.php

--- index.php
+++ index.php
@@ -1,18 +1,7 @@
 <?php
+if ($init){
 include_once("rss.php");
+}
+
 include_once("connection.php");
 
-$rssInfo = getRss();
-
-foreach ($rssInfo as $class) {
-	echo "<pre>";
-	print_r($class);
-	//echo $class['title'] . " " . $class[''] . "<br />";
-	echo "</pre>";
-	$stmt = $db_stickers->prepare("INSERT INTO classes (classname) VALUES (?)");
-    $stmt->bind_param('s', $class['title']);
-    $stmt->execute();
-    $stmt->close();
-
-}
-?>
--- rss.php
+++ rss.php
@@ -1,4 +1,22 @@
 <?php
+
+include_once("connection.php");
+
+$rssInfo = getRss();
+
+foreach ($rssInfo as $class) {
+	echo "<pre>";
+	print_r($class);
+	//echo $class['title'] . " " . $class[''] . "<br />";
+	echo "</pre>";
+	$stmt = $db_stickers->prepare("INSERT INTO classes (classname) VALUES (?)");
+    $stmt->bind_param('s', $class['title']);
+    $stmt->execute();
+    $stmt->close();
+
+}
+
+
 function rss_to_array($tag, $array, $url) {
 		$doc = new DOMdocument();
 		$doc->load($url);

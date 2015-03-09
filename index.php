c84cdbef7afffc9836d8f07d9988ea7b6db67d02
Milo Rupp <milo.rupp@pscs.org>
2015-03-09 08:45:10 -0700

    it now tables

--- index.php
+++ index.php
@@ -1,7 +1,36 @@
 <?php
+$init = 0;
 if ($init){
 include_once("rss.php");
 }
 
 include_once("connection.php");
 
+$result = $db_stickers->query("SELECT * FROM classes");
+$classesresult = array();
+while ($data_result = $result->fetch_assoc()) {
+	array_push($classesresult, $data_result);
+}
+	
+?>
+<table>
+<tr>
+<th> class </th>
+<th> facilitator </th>
+<th> block </th>
+<th> black stickers </th>
+<th> grey stickers </th>
+<th> white stickers </th>
+</tr>
+<?php
+foreach($classesresult as $class){
+?> <tr>
+<td> <?php echo $class['classname']; ?> </td>
+<td> <?php echo $class['facilitatorid']; ?> </td>
+<td> <?php echo $class['block']; ?> </td>
+<td> <?php echo $class['blackstickers']; ?> </td>
+<td> <?php echo $class['greystickers']; ?> </td>
+<td> <?php echo $class['whitestickers']; ?> </td>
+</tr>
+<?php
+}

6ffec81991ca7fa2d911ece6e07fa74f2ac24c3e
Badtz Maru <badtzmaru139@gmail.com>
2015-04-22 09:34:30 -0700

    uncommented the insert

--- rss.php
+++ rss.php
@@ -14,12 +14,11 @@ foreach ($rssInfo as $class) {
 	echo "<pre>";
 	print_r($class);
 	echo "</pre>";
-	/*
+
 	$stmt = $db_stickers->prepare("INSERT INTO classes (classname) VALUES (?)");
     $stmt->bind_param('s', $class['title']);
     $stmt->execute();
     $stmt->close();
-	*/
 
 }
 

cd9d99d0f07cfaa694bf49fe2d84722e0d3d2e02
Badtz Maru <badtzmaru139@gmail.com>
2015-05-24 09:01:22 -0700

    testfunctions should work now

--- testfunctions.php
+++ testfunctions.php
@@ -1,6 +1,6 @@
 <?php
 function addsticker($studentid,$classid,$stickertype){
-	echo $studentid . " " . $classid . " " . $stickertype . "<br>";
+	// $studentid . " " . $classid . " " . $stickertype . "<br>";
 	global $db_attendance;
 	global $db_stickers;
 	$zero = 0;
@@ -19,37 +19,47 @@ function addsticker($studentid,$classid,$stickertype){
 		array_push($stickercolors,"blackstickers","greystickers","whitestickers");
 		
 		foreach($stickercolors as $color){
-			echo $rowresult[0][$color] . "<br>";
+			// $rowresult[0][$color] . "<br>";
 			
 			if(strcmp($rowresult[0][$color],$studentid) == 0){
-				echo " cell is the same as studentid";
+				// " cell is the same as studentid";
 				if($color == $stickertype){
-					echo "was of selected color so remove";
+					// "was of selected color so remove";
 					$stmt = $db_stickers->prepare("UPDATE offerings SET $stickertype = ? WHERE classid=$classid");
 					$stmt->bind_param('s', $zero);
 					$stmt->execute();
 				} else {
-					echo " already stickered in different color";
+					// " already stickered in different color";
 					break;
 				}
 			} elseif ($rowresult[0][$color] == 0){
-				echo " no stickers";
+				// " no stickers";
 				if($color == $stickertype){
-					echo "zero so just add";
+					// "zero so just add";
 					$stmt = $db_stickers->prepare("UPDATE offerings SET $stickertype = ? WHERE classid=$classid");
 					$stmt->bind_param('s', $studentid);
 					$stmt->execute();
 				}
 			} elseif(strpos($rowresult[0][$color],",")) {
 				$celldata = explode(",",$rowresult[0][$color]);
-				echo "<pre>";
+				// "<pre>";
 				print_r($celldata);
-				echo "</pre>"; 
+				// "</pre>"; 
 				if (in_array($studentid,$celldata)){
-					echo "already in " . $color . " " . $stickertype;
-					if($color === $stickertype){
-						"already stickered here so remove";
-						unset($celldata[$color]);
+					// "already in " . $color . " " . $stickertype;
+					if($color == $stickertype){
+						// "already stickered here so remove";
+						$key = array_search($studentid,$celldata);
+						unset($celldata[$key]);
+						$celldata = implode(",", $celldata);
+						$stmt = $db_stickers->prepare("UPDATE offerings SET $stickertype = ? WHERE classid=$classid");
+						$stmt->bind_param('s', $celldata);
+						$stmt->execute();
+					}
+				} else {
+					if($color == $stickertype){
+						// "not stickered but should be ";
+						array_push($celldata, $studentid);
 						$celldata = implode(",", $celldata);
 						$stmt = $db_stickers->prepare("UPDATE offerings SET $stickertype = ? WHERE classid=$classid");
 						$stmt->bind_param('s', $celldata);

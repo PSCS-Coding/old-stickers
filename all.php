17e02f34d5c87df9ae1adcdd8ad3c1445e2dc3c8
eastonkugler <eastonkugler@gmail.com>
2015-08-03 10:59:50 -0700

    Generates a styled sticker table for each class

--- all.php
+++ all.php
@@ -13,7 +13,40 @@ while($class = $classesQuery->fetch_assoc()) {
 	array_push($classesResult, $class);
 }
 foreach($classesResult as $sub) {
-	echo $sub["classname"] . "<br />";
+	//echo $sub["classname"];
 	//table
+	echo "<table name='" . $sub["classid"] . "'>
+			<caption>" . $sub["classname"] . "</caption>
+			<colgroup>
+				<col class='blackstickers'>
+				<col class='greystickers'>
+				<col class='whitestickers'>
+			</colgroup>
+			<tr>
+				<th class='black'>Black</th>
+				<th>Grey</th>
+				<th>White</th>
+			</tr>
+			<tr>
+				<td class='black'>Hello</td><td>world</td><td>!!!</td> 
+			</tr>
+		  </table>";
 }
 ?>
+<style>
+table, th, td {
+    border: 1px solid black;
+}
+.blackstickers {
+	background-color:black;
+}
+.greystickers {
+	background-color:#A5A5A5;
+}
+.whitestickers {
+	background-color:white;
+}
+.black {
+	color:white;
+}
+</style>

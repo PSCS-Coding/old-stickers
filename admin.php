e3da51c7bc5da4a633c30f997d8fd00bcabade7c
eastonkugler <eastonkugler@gmail.com>
2015-08-03 10:18:36 -0700

    Fixed database insertion and value display
    Should be working fine now

--- admin.php
+++ admin.php
@@ -8,44 +8,45 @@ require_once("connection.php");
 <head>
 <div id="all">
 	<div id="container" style="max-width:800px;margin:1em auto 0;background-color:lightgrey;border-radius:15px;height:500px">
+		<!-- <p id="allottedtext">Allotted Stickers</p> -->
 		<div id="allottedstickers">
 			<?php
 			//here
 			if (!empty($_POST['submitstickers'])) {
 				$stmt = $db_stickers->prepare("UPDATE allottedstickers SET blackstickers = ?, greystickers = ?, whitestickers = ?");
-				$stmt->bind_param('iii', $_POST['black'], $_POST['grey'], $_POST['white']);
+				$stmt->bind_param('iii', $_POST['blackAllotted'], $_POST['greyAllotted'], $_POST['whiteAllotted']);
 				$stmt->execute();
 				$stmt->close();	
 			}
 			$stickersQuery = $db_stickers->query("SELECT * FROM allottedstickers LIMIT 1");
-			$stickerWeightQuery = $db_stickers->query("SELECT * FROM stickerweight LIMIT 1");
 			$stickersResult = $stickersQuery->fetch_array();
-			?><form method="post">
+			?><form name="stickerform" method="post">
 				<div class="rangecontent">
-				<div id="blackdiv" class="stickerdiv">Black Stickers</div><input type="range" id="black" class="ranged" name="black" min="0" max="10" step="1" value="<?php echo $stickersResult['blackstickers'] ?>" oninput="updateTextInput(this.value, 'blackS');"><span class="label" id="blackS"><?php echo " " . $stickersResult['blackstickers'] ?></span><br /><br />
-				<div id="greydiv"  class="stickerdiv">Grey Stickers</div> <input type="range" id="grey"  class="ranged" name="grey"  min="0" max="10" step="1" value="<?php echo $stickersResult['greystickers']  ?>" oninput="updateTextInput(this.value, 'greyS');"><span class="label" id="greyS"><?php echo $stickersResult['greystickers'] ?></span><br /><br />
-				<div id="whitediv" class="stickerdiv">White Stickers</div><input type="range" id="white" class="ranged" name="white" min="0" max="10" step="1" value="<?php echo $stickersResult['whitestickers'] ?>" oninput="updateTextInput(this.value, 'whiteS');"><span class="label" id="whiteS"><?php echo $stickersResult['whitestickers'] ?></span><br /><br />
+				<div id="blackdiv" class="stickerdiv">Black Stickers</div><input type="range" id="black" class="ranged" name="blackAllotted" min="0" max="10" step="1" value="<?php echo $stickersResult['blackstickers'] ?>" oninput="updateTextInput(this.value, 'blackS');"><span class="label" id="blackS"><?php echo " " . $stickersResult['blackstickers'] ?></span><br /><br />
+				<div id="greydiv"  class="stickerdiv">Grey Stickers</div> <input type="range" id="grey"  class="ranged" name="greyAllotted"  min="0" max="10" step="1" value="<?php echo $stickersResult['greystickers']  ?>" oninput="updateTextInput(this.value, 'greyS');"><span class="label" id="greyS"><?php echo $stickersResult['greystickers'] ?></span><br /><br />
+				<div id="whitediv" class="stickerdiv">White Stickers</div><input type="range" id="white" class="ranged" name="whiteAllotted" min="0" max="10" step="1" value="<?php echo $stickersResult['whitestickers'] ?>" oninput="updateTextInput(this.value, 'whiteS');"><span class="label" id="whiteS"><?php echo $stickersResult['whitestickers'] ?></span><br /><br />
 				<input type="submit" name="submitstickers" value="submit">
 				</div>
 			</form>
 		</div>
 		
+		<!-- <p id="weighttext">Sticker Weight</p> -->
 		<div id="stickerweight">
 			<?php
 			//sticker weight
 			if (!empty($_POST['submitweight'])) {
-				$stmt = $db_stickers->prepare("UPDATE allottedstickers SET blackstickers = ?, greystickers = ?, whitestickers = ?");
-				$stmt->bind_param('iii', $_POST['black'], $_POST['grey'], $_POST['white']);
+				$stmt = $db_stickers->prepare("UPDATE stickerweight SET blackvalue = ?, greyvalue= ?, whitevalue = ?");
+				$stmt->bind_param('ddd', $_POST['blackWeight'], $_POST['greyWeight'], $_POST['whiteWeight']);
 				$stmt->execute();
 				$stmt->close();	
 			}
-			$stickersQuery = $db_stickers->query("SELECT * FROM allottedstickers LIMIT 1");
-			$stickersResult = $stickersQuery->fetch_array();
-			?><form method="post">
+			$weightQuery = $db_stickers->query("SELECT * FROM stickerweight LIMIT 1");
+			$weightResult = $weightQuery->fetch_array();
+			?><form name="weightform" method="post">
 				<div class="rangecontent">
-				<div id="blackdivW" class="stickerdiv">Black Stickers</div><input type="range" id="black" class="ranged" name="black" min="0" max="15" step=".5" value="<?php echo $stickersResult['blackstickers'] ?>" oninput="updateTextInput(this.value, 'blackW');"><span id="blackW" class="label"><?php echo " " . $stickersResult['blackstickers'] ?></span><br /><br />
-				<div id="greydivW"  class="stickerdiv">Grey Stickers</div> <input type="range" id="grey"  class="ranged" name="grey"  min="0" max="15" step=".5" value="<?php echo $stickersResult['greystickers']  ?>" oninput="updateTextInput(this.value, 'greyW');"><span id="greyW" class="label"><?php echo $stickersResult['greystickers'] ?></span><br /><br />
-				<div id="whitedivW" class="stickerdiv">White Stickers</div><input type="range" id="white" class="ranged" name="white" min="0" max="15" step=".5" value="<?php echo $stickersResult['whitestickers'] ?>" oninput="updateTextInput(this.value, 'whiteW');"><span id="whiteW" class="label"><?php echo $stickersResult['whitestickers'] ?></span><br /><br />
+				<div id="blackdivW" class="stickerdiv">Black Stickers</div><input type="range" id="black" class="ranged" name="blackWeight" min="0" max="15" step=".5" value="<?php echo $weightResult['blackvalue'] ?>" oninput="updateTextInput(this.value, 'blackW');"><span id="blackW" class="label"><?php echo " " . $weightResult['blackvalue'] ?></span><br /><br />
+				<div id="greydivW"  class="stickerdiv">Grey Stickers</div> <input type="range" id="grey"  class="ranged" name="greyWeight"  min="0" max="15" step=".5" value="<?php echo $weightResult['greyvalue']  ?>" oninput="updateTextInput(this.value, 'greyW');"><span id="greyW" class="label"><?php echo $weightResult['greyvalue'] ?></span><br /><br />
+				<div id="whitedivW" class="stickerdiv">White Stickers</div><input type="range" id="white" class="ranged" name="whiteWeight" min="0" max="15" step=".5" value="<?php echo $weightResult['whitevalue'] ?>" oninput="updateTextInput(this.value, 'whiteW');"><span id="whiteW" class="label"><?php echo $weightResult['whitevalue'] ?></span><br /><br />
 				<input type="submit" name="submitweight" value="submit">
 				</div>
 			</form>
@@ -61,6 +62,10 @@ document.getElementById(span).innerHTML = val;
 <style>
 * {
 font-family:sans-serif;
+}
+#allottedtext {
+}
+#weighttext {
 }
 #stickerweight {
 height:25%;

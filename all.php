537d4f7ec98fa73ae510e2f56c7f0cf19b3e209e
eastonkugler <eastonkugler@gmail.com>
2015-08-03 18:51:55 -0700

    Extensive styling to make everything more readable
    Including Caption scroll overflow and background, background image, and
    table alignment

--- all.php
+++ all.php
@@ -6,6 +6,7 @@ require_once("connection.php");
 <head>
 	<title>All Classes</title>
 	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="stickers.css">  
 </head>
 <?php
 $classesQuery = $db_stickers->query("SELECT * FROM offerings");
@@ -13,11 +14,14 @@ $classesResult = array();
 while($class = $classesQuery->fetch_assoc()) {
 	array_push($classesResult, $class);
 }
+?>
+<!-- Enclosing Classes Table -->
+<?php
 foreach($classesResult as $sub) {
 	//echo $sub["classname"];
 	//table
-	echo "<table name='" . $sub["classid"] . "'>
-			<caption>" . $sub["classname"] . "</caption>
+	?>	<table name= <?php echo $sub["classid"] ?> >
+			<caption> <?php echo $sub["classname"] ?> </caption>
 			<colgroup>
 				<col class='blackstickers'>
 				<col class='greystickers'>
@@ -25,23 +29,33 @@ foreach($classesResult as $sub) {
 			</colgroup>
 			<tr>
 				<th class='black'>Black</th>
-				<th>Grey</th>
-				<th>White</th>
+				<th class='grey'>Grey</th>
+				<th class='white'>White</th>
 			</tr>
 			<tr>
 				<td class='black'>Hello</td><td>world</td><td>!!!</td> 
 			</tr>
-		  </table>";
+		 </table>
+	<?php
 }
 ?>
 <style>
-table, th, td {
+table, th, td, caption {
     border: 1px solid black;
 }
 table {
 	float:left;
 	margin:1%;
 	width:10%;
+}
+caption {
+	opacity: 0.5;
+    overflow-y: scroll;
+    background-color: white;
+    width: 100%;
+    height: 2%;
+	padding-top:1%;
+	padding-bottom:2%;
 }
 .blackstickers {
 	background-color:black;

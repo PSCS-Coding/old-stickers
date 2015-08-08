09bab9786970a4fe6bb7dbb6535062d7c5cdd0e7
eastonkugler <eastonkugler@gmail.com>
2015-08-08 13:30:21 -0700

    Shows table with days of the week

---
+++ edit_schedule.php
@@ -0,0 +1,27 @@
+<DOCTYPE HTML>
+<html>
+<head>
+	<title>Edit Schedule</title>
+	<!--<link rel="stylesheet" type="text/css" href="stickers.css">-->
+</head>
+<?php
+	include_once("connection.php");
+	include_once("stickerfunctions.php");
+	include_once("function.php");
+?>
+<body>
+	<table id="schedule">
+		<caption>Schedule Visualization</caption>
+			<colgroup>
+				<col span=5 style="background-color:azure">
+			</colgroup>
+		<tr>
+			<td>Monday</td>
+			<td>Tuesday</td>
+			<td>Wednesday</td>
+			<td>Thursday</td>
+			<td>Friday</td>
+		</tr>
+	</table>
+</body>
+</html>

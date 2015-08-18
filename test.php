55c1b72b99ab8500ed269f102f3e86a910edbadb
Badtz Maru <badtzmaru139@gmail.com>
2015-08-17 22:55:34 -0700

    blocks now get pulled in the reset

--- test.php
+++ test.php
@@ -3,5 +3,5 @@ include_once("connection.php");
 include_once("function.php");
 include_once("stickerfunctions.php");
 
-get_teacher("http://classes.pscs.org/2015/02/graphic-design-101/");
+is_block("http://classes.pscs.org/2015/02/fix-it-finish-it-build-it-hang-it/");
 ?>

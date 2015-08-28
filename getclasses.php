de12282dbb7b8f8a74ac4a1d161b2d36d0d0b5db
eastonkugler <eastonkugler@gmail.com>
2015-08-28 10:37:29 -0700

    getclasses.php returns classes on slot passed to it

--- getclasses.php
+++ getclasses.php
@@ -1,4 +1,7 @@
 <?php
-echo $_GET["slot"];
+include_once("connection.php");
 
+$slot = $_GET["slot"];
+$classQuery = $db_stickers->query("SELECT property FROM scheduledata WHERE id ='$slot' LIMIT 1");
+echo $classQuery->fetch_array()[0];
 ?>
